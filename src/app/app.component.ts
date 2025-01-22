0644154bb1999a075e896436697983111b1920ec
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-01-22 10:09:56 +0100

    Creado componente listado y enrutado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ListadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/listado', component: ListadoComponent }
+];
