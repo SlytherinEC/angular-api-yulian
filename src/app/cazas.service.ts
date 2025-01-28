9a23187e1ab46818dd02790bc0a4e89c5c803606
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-01-28 11:00:37 +0100

    modificado CazaService para que id sea string ya que la api inserta ids de tipo String

--- src/app/cazas.service.ts
+++ src/app/cazas.service.ts
@@ -1,40 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CazasService {
 
-  private baseUrl = 'https://cazas.free.beeceptor.com';
-  // private baseUrl = 'https://cazas.free.beeceptor.com/api';
+  private baseUrl = 'https://cazas.free.beeceptor.com/api';
+  // private baseUrl = 'http://localhost:3000/api';
+  idCaza: String = '';
 
   constructor(private http: HttpClient) { }
 
   // Obtener todos los cazas
   getCazas(): Observable<any> {
-    return this.http.get<any>(`https://cazas.free.beeceptor.com/api/cazas`);
+    return this.http.get<any>(`${this.baseUrl}/cazas`);
   }
 
-  // Obtener un caza por id
-  getCazaById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/cazas/${id}`);
-  }
-
+// Obtener un caza por ID utilizando Promises
+getCazaById(id: String): Promise<any> {
+  return firstValueFrom(this.http.get<any>(`${this.baseUrl}/cazas/${id}`));
+}
   // Crear un caza
   createCaza(caza: any): Observable<any> {
-    return this.http.post(`https://cazas.free.beeceptor.com/api/cazas`, caza);
+    return this.http.post(`${this.baseUrl}/cazas`, caza);
   }
 
   // Actualizar un caza
-  updateCaza(id: number, caza: any): Observable<any> {
+  updateCaza(id: String, caza: any): Observable<any> {
     return this.http.put(`${this.baseUrl}/cazas/${id}`, caza);
   }
 
   // Eliminar un caza
-  deleteCaza(id: number): Observable<any> {
-    return this.http.delete(`https://cazas.free.beeceptor.com/api/cazas/${id}`);
+  deleteCaza(id: String): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/cazas/${id}`);
   }
 
 }
